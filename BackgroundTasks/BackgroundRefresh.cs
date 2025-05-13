c497d72147c9fb397117fbce0b68c18d7359cfaf
Joe Moore <joebilly8@hotmail.co.uk>
2025-05-13 19:26:23 +1000

    add sample data boilerplate

---
+++ BackgroundTasks/BackgroundRefresh.cs
@@ -0,0 +1,21 @@
+﻿
+namespace BackgroundTasks
+{
+    public class BackgroundRefresh : IHostedService, IDisposable
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BackgroundTasks/SampleData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Concurrent;
+
+namespace BackgroundTasks
+{
+    // Pretend this is our cache for simplicity purposes
+    public class SampleData
+    {
+        public ConcurrentBag<string> Data { get; set; } = new();
+    }
+}
